b2117e280794d4b90106fefec3ad4effb2b74f25
yanhaijing <yanhaijing@yeah.net>
2015-12-28 13:18:54 +0800

    删除无用文件，测试jqui

--- modules/app/index.js
+++ modules/app/index.js
@@ -1,22 +1,11 @@
 var $ = require('jquery');
+require('jquery-ui/autocomplete');
 
-// 测试UIbase
-var UIBase = require('ui/base/base');
-console.log(UIBase);
-
-// 测试Promise
-var Promise = require('es6-promise').Promise;
-console.log(Promise);
-
-// 测试template
-require('util/template/template');
-var tpl = __inline('test.tmpl');
-$('body').append(tpl({}));
-
-// 测试 zepto
-var zepto = require('zepto');
-console.log(zepto);
-
-// 测试 underscore
-var _ = require('underscore');
-console.log(_)
+var availableTags = [];
+Object.keys(window).forEach(function (v) {
+    availableTags.push(v);
+    console.log(v);
+});
+$("#aio").autocomplete({
+    source: availableTags
+});
