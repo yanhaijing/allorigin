26d92e10bba9ab26fd6f8ee847b2e9617c61e69f
yanhaijing <yanhaijing@yeah.net>
2015-12-30 08:21:28 +0800

    修改样式
    1. 完成页头样式
    2. 完成输入框样式

--- modules/app/index.js
+++ modules/app/index.js
@@ -6,6 +6,19 @@ Object.keys(window).forEach(function (v) {
     availableTags.push(v);
     console.log(v);
 });
+
+
 $("#aio").autocomplete({
     source: availableTags
+}).on('keypress', function (e) {
+    if (e.keyCode !== 13) {
+        return 1;
+    }
+
+    // 回车
+    var term = $(this).val().trim();
+    if (term === '') {
+        return 2;
+    }
+    console.log(term);
 });
